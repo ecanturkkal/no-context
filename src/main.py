3d0dc2761a0de83e940eb819c759e3aa97805818
eyupcanturkkal <eyupcanturkkal@vituscommodities.com>
2024-01-16 17:21:42 +0300

    fixes

--- src/main.py
+++ src/main.py
@@ -1,4 +1,6 @@
 import uvicorn
+import logging
+from logstash_async.handler import AsynchronousLogstashHandler
 from fastapi import FastAPI
 from src.routers import hello_world, todos
 
@@ -7,9 +9,14 @@ def get_application() -> FastAPI:
     application = FastAPI()
     application.include_router(hello_world.router)
     application.include_router(todos.router)
-
+    logger.info("Hello world! Api is started.")
     return application
 
+
+logger = logging.getLogger("no-context-api-logs")
+logger.setLevel(logging.INFO)
+logstash_handler = AsynchronousLogstashHandler("localhost", 5000, database_path=None)
+logger.addHandler(logstash_handler)
 
 app = get_application()
 
